4291412264fd604ce8b227c6e8dca32debd0e523
Eko Eryanto <ekoeryanto@gmail.com>
2024-12-24 15:32:25 +0700

    chore: remove duplicate css

--- pages/index.vue
+++ pages/index.vue
@@ -157,14 +157,3 @@ const { data } = await useAsyncData(
     </v-footer>
   </v-main>
 </template>
-
-<style>
-.grad-1 {
-  background: rgb(2,0,36);
-  background: radial-gradient(circle, rgba(2,0,36,1) 0%, rgba(8,23,252,1) 0%, rgba(0,212,255,1) 100%);
-}
-.grad-0 {
-  background: rgb(40,23,112) !important;
-  background: linear-gradient(0deg, rgba(40,23,112,1) 0%, rgba(4,135,210,1) 100%) !important;
-}
-</style>
