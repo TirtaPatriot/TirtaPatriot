92d2ed4b608baac73f770c3e48e8f8841651a71e
Eko Eryanto <ekoeryanto@gmail.com>
2024-12-24 15:31:27 +0700

    chore: move global style to app.vue

--- app.vue
+++ app.vue
@@ -19,4 +19,14 @@
   background: rgb(34, 120, 195);
   background: linear-gradient(0deg, rgb(34, 120, 195) 0%, rgba(255,255,255,1) 100%);
 }
+
+.grad-1 {
+  background: rgb(2,0,36);
+  background: radial-gradient(circle, rgba(2,0,36,1) 0%, rgba(8,23,252,1) 0%, rgba(0,212,255,1) 100%);
+}
+
+.grad-0 {
+  background: rgb(40,23,112) !important;
+  background: linear-gradient(0deg, rgba(40,23,112,1) 0%, rgba(4,135,210,1) 100%) !important;
+}
 </style>
