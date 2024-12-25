f08ad1ee7354c3d7ea63323d53324e18eaef55f3
Eko Eryanto <ekoeryanto@gmail.com>
2024-12-25 11:13:17 +0700

    Update nuxt.config.ts cannonical redirect

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -80,7 +80,7 @@ export default defineNuxtConfig({
   },
   seo: {
     splash: false,
-    // redirectToCanonicalSiteUrl: true,
+    redirectToCanonicalSiteUrl: true,
   },
   sitemap: {
     enabled: true,
