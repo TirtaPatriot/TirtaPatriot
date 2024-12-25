ad8be9d3e08c2ff7435c9522f7f30f759360f888
Eko Eryanto <ekoeryanto@gmail.com>
2024-12-25 11:15:40 +0700

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -80,7 +80,7 @@ export default defineNuxtConfig({
   },
   seo: {
     splash: false,
-    redirectToCanonicalSiteUrl: true,
+    redirectToCanonicalSiteUrl: false,
   },
   sitemap: {
     enabled: true,
