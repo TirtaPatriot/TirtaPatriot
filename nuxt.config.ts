5a74f2d86cfd4c44e06ac4448b64adfcb5e99f21
Eko Eryanto <ekoeryanto@gmail.com>
2026-04-03 01:43:54 +0700

    chore: disable devtools

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -34,9 +34,15 @@ const articleRoutes = await loadArticleRoutesFromDirectus()
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-12-21',
-  devtools: { enabled: true },
+  devtools: { enabled: process.env.NODE_ENV !== 'production' },
+
+  sourcemap: {
+    client: false,
+    server: false,
+  },
 
   app: {
+    buildAssetsDir: '/assets/',
     head: {
       htmlAttrs: {
         lang: 'id',
